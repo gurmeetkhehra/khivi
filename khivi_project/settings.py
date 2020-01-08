0644bb676c5692695cb053812c2394e728c38523
jasminekhehra <khehragurmeet5@gmail.com>
2020-01-07 16:15:57 -0800

    New updates for Heroku deployment

--- khivi_project/settings.py
+++ khivi_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mgpb425wlhcfey6ij*&ewb@toaba(xizzo_xdpxyg6!1=j&ih0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
